af013976452f8803405f4c60a124bb167d8a5a71
KhilarJitendra <jitendra.khilar@akqa.com>
2023-03-09 22:59:10 +0530

    Files Added

--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,46 @@
 const express = require("express");
 const router = express.Router();
+const request = require("request");
+const csv = require("csv-parser");
+const fs = require("fs");
+const XLSX = require("xlsx");
+const { json } = require("express");
+const axios = require("axios");
 
 router.get("/", async (req, res, next) => {
-  return res.status(200).json({
-    title: "Express Testing",
-    message: "The app is working properly!",
+  fs.readFile("extractor-config.json", "utf8", (err, data) => {
+    const configData = JSON.parse(data);
+
+    // Read the Google Sheet
+
+    const axios = require("axios");
+    const XLSX = require("xlsx");
+    const url = configData.sheets[0].url;
+    const owner = configData.assignedTo;
+
+    axios
+      .get(url, {
+        responseType: "arraybuffer",
+      })
+      .then((response) => {
+        const workbook = XLSX.read(response.data, { type: "buffer" });
+        const sheetName = workbook.SheetNames[0];
+        const worksheet = workbook.Sheets[sheetName];
+        const data = XLSX.utils.sheet_to_json(worksheet);
+
+        let actualData = [];
+
+        data.forEach((item) => {
+          if (item.Owner == owner) {
+            actualData.push(item);
+          }
+        });
+
+        res.json(actualData);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   });
 });
 
