1879c751d2e85fa21645c566d259fc8ac656f99a
KhilarJitendra <jitendra.khilar@akqa.com>
2023-03-10 02:36:01 +0530

    connection34

--- routes/home.js
+++ routes/home.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const request = require("request");
+const csv = require("csv-parser");
 const fs = require("fs-extra");
 const XLSX = require("xlsx");
 // const { JSON } = require("express");
@@ -22,55 +24,52 @@ router.get("/", async (req, res, next) => {
 
   fs.readFile('extractor-config.json')
   .then(data => {
-    // console.log('bufferdata......', JSON.parse(data));
-    // const parseddata = JSON.parse(data);
-    // url = parseddata.sheets[0].url;
-    // owner = parseddata.assignedTo;
-    // console.log(owner);
-
-    return res.json({
-      "jitendra": "123123"
-    });
-
-
-    
-    
-    
+    console.log('bufferdata......', JSON.parse(data));
+    const parseddata = JSON.parse(data);
+    url = parseddata.sheets[0].url;
+    owner = parseddata.assignedTo
+    console.log(parseddata)
   })
   .catch(err => {
     console.error(err);
   });
-  
-  
-  
-  
-  // axios
-  // .get("https://docs.google.com/spreadsheets/d/e/2PACX-1vTll1DTciIbS7lsdkjYmgrFnhnD4AJlmtq8u9AZOOAJDWdHzpkdVVbMRXQPNVGXvyFvzMNvv1C7O5JO/pubhtml", {
-  //   responseType: "arraybuffer",
-  // })
-  // .then((response) => {
-  //   const workbook = XLSX.read(response.data, { type: "buffer" });
-  //   const sheetName = workbook.SheetNames[0];
-  //   const worksheet = workbook.Sheets[sheetName];
-  //   const data = XLSX.utils.sheet_to_json(worksheet);
 
-  //   let actualData = [];
-
-  //   data.forEach((item) => {
-  //     if (item.Owner == owner) {
-  //       actualData.push(item);
-  //     }
-  //   });
-
-  //   return res.json(actualData);
-
-    
-  // })
-  // .catch((error) => {
-  //   console.error(error);
-  // });
-  
  
+
+  // Read the Google Sheet
+
+  // console.log('configData..',jsonData)
+
+  // const axios = require("axios");
+  // const XLSX = require("xlsx");
+  // const url = configData.sheets[0].url;
+  // const owner = configData.assignedTo;
+
+  axios
+    .get(url, {
+      responseType: "arraybuffer",
+    })
+    .then((response) => {
+      const workbook = XLSX.read(response.data, { type: "buffer" });
+      const sheetName = workbook.SheetNames[0];
+      const worksheet = workbook.Sheets[sheetName];
+      const data = XLSX.utils.sheet_to_json(worksheet);
+
+      let actualData = [];
+
+      data.forEach((item) => {
+        if (item.Owner == owner) {
+          actualData.push(item);
+        }
+      });
+
+      return res.json(actualData);
+
+      
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 });
 
 module.exports = router;
