87444f58fd921be948912e31f53aeeaa4f233d2c
KhilarJitendra <jitendra.khilar@akqa.com>
2023-03-09 22:47:27 +0530

    Test Connection

--- index.js
+++ index.js
@@ -1,54 +1,14 @@
+// Import packages
 const express = require("express");
-const request = require("request");
-const csv = require("csv-parser");
-const fs = require("fs");
-const XLSX = require("xlsx");
-const { json } = require("express");
-const axios = require("axios");
+const home = require("./routes/home");
 
+// Middlewares
 const app = express();
+app.use(express.json());
 
-app.get("/data", (req, res) => {
-  // Read The config File
+// Routes
+app.use("/home", home);
 
-  fs.readFile("extractor-config.json", "utf8", (err, data) => {
-    const configData = JSON.parse(data);
-
-    // Read the Google Sheet
-
-    const axios = require("axios");
-    const XLSX = require("xlsx");
-    const url = configData.sheets[0].url;
-    const owner = configData.assignedTo;
-
-    axios
-      .get(url, {
-        responseType: "arraybuffer",
-      })
-      .then((response) => {
-        const workbook = XLSX.read(response.data, { type: "buffer" });
-        const sheetName = workbook.SheetNames[0];
-        const worksheet = workbook.Sheets[sheetName];
-        const data = XLSX.utils.sheet_to_json(worksheet);
-
-        let actualData = [];
-
-        data.forEach((item) => {
-          if (item.Owner == owner) {
-            actualData.push(item);
-          }
-        });
-
-        res.json(actualData);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  });
-});
-
+// connection
 const port = process.env.PORT || 9001;
-
-app.listen(port, () => {
-  console.log("Server running on port 3000");
-});
+app.listen(port, () => console.log(`Listening to port ${port}`));
