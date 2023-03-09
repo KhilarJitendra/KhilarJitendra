8705fd376f8c6c60ed7cc1a96f4daff83541cc3a
KhilarJitendra <jitendra.khilar@akqa.com>
2023-03-09 23:35:51 +0530

    test connection 2

--- routes/home.js
+++ routes/home.js
@@ -1,50 +1,63 @@
+// const express = require("express");
+// const router = express.Router();
+// const fs = require("fs");
+// const XLSX = require("xlsx");
+// const { json } = require("express");
+// const axios = require("axios");
+
+// router.get("/", async (req, res, next) => {
+
+//   console.log('Req....')
+
+//   // fs.readFile("extractor-config.json", "utf8", (err, data) => {
+//   //   const configData = JSON.parse(data);
+
+//   //   // Read the Google Sheet
+
+//   //   const axios = require("axios");
+//   //   const XLSX = require("xlsx");
+//   //   const url = configData.sheets[0].url;
+//   //   const owner = configData.assignedTo;
+
+//   //   console.log('Dataa are...',data)
+
+//   //   // axios
+//   //   //   .get(url, {
+//   //   //     responseType: "arraybuffer",
+//   //   //   })
+//   //   //   .then((response) => {
+//   //   //     const workbook = XLSX.read(response.data, { type: "buffer" });
+//   //   //     const sheetName = workbook.SheetNames[0];
+//   //   //     const worksheet = workbook.Sheets[sheetName];
+//   //   //     const data = XLSX.utils.sheet_to_json(worksheet);
+
+//   //   //     let actualData = [];
+
+//   //   //     data.forEach((item) => {
+//   //   //       if (item.Owner == owner) {
+//   //   //         actualData.push(item);
+//   //   //       }
+//   //   //     });
+
+//   //   //     res.json(actualData);
+//   //   //   })
+//   //   //   .catch((error) => {
+//   //   //     console.error(error);
+//   //   //   });
+//   // });
+// });
+
+// module.exports = router;
+
+
 const express = require("express");
 const router = express.Router();
-const fs = require("fs");
-const XLSX = require("xlsx");
-const { json } = require("express");
-const axios = require("axios");
 
 router.get("/", async (req, res, next) => {
-
-  console.log('Req....')
-
-  // fs.readFile("extractor-config.json", "utf8", (err, data) => {
-  //   const configData = JSON.parse(data);
-
-  //   // Read the Google Sheet
-
-  //   const axios = require("axios");
-  //   const XLSX = require("xlsx");
-  //   const url = configData.sheets[0].url;
-  //   const owner = configData.assignedTo;
-
-  //   console.log('Dataa are...',data)
-
-  //   // axios
-  //   //   .get(url, {
-  //   //     responseType: "arraybuffer",
-  //   //   })
-  //   //   .then((response) => {
-  //   //     const workbook = XLSX.read(response.data, { type: "buffer" });
-  //   //     const sheetName = workbook.SheetNames[0];
-  //   //     const worksheet = workbook.Sheets[sheetName];
-  //   //     const data = XLSX.utils.sheet_to_json(worksheet);
-
-  //   //     let actualData = [];
-
-  //   //     data.forEach((item) => {
-  //   //       if (item.Owner == owner) {
-  //   //         actualData.push(item);
-  //   //       }
-  //   //     });
-
-  //   //     res.json(actualData);
-  //   //   })
-  //   //   .catch((error) => {
-  //   //     console.error(error);
-  //   //   });
-  // });
+  return res.status(200).json({
+    title: "Express Testing",
+    message: "The app is working properly!",
+  });
 });
 
 module.exports = router;
