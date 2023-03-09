e6f9e3eabf49d574da656abfc2fe62ecefc1ba31
KhilarJitendra <jitendra.khilar@akqa.com>
2023-03-10 00:07:29 +0530

    connection11

--- routes/home.js
+++ routes/home.js
@@ -9,47 +9,39 @@ const axios = require("axios");
 
 router.get("/", async (req, res, next) => {
   fs.readFile("extractor-config.json", "utf8", (err, data) => {
-    // const configData = JSON.parse(data);
+    const configData = JSON.parse(data);
 
-    // // Read the Google Sheet
+    // Read the Google Sheet
 
-    // const axios = require("axios");
-    // const XLSX = require("xlsx");
-    // const url = configData.sheets[0].url;
-    // const owner = configData.assignedTo;
+    const axios = require("axios");
+    const XLSX = require("xlsx");
+    const url = configData.sheets[0].url;
+    const owner = configData.assignedTo;
 
-    // axios
-    //   .get(url, {
-    //     responseType: "arraybuffer",
-    //   })
-    //   .then((response) => {
-    //     const workbook = XLSX.read(response.data, { type: "buffer" });
-    //     const sheetName = workbook.SheetNames[0];
-    //     const worksheet = workbook.Sheets[sheetName];
-    //     const data = XLSX.utils.sheet_to_json(worksheet);
+    axios
+      .get(url, {
+        responseType: "arraybuffer",
+      })
+      .then((response) => {
+        const workbook = XLSX.read(response.data, { type: "buffer" });
+        const sheetName = workbook.SheetNames[0];
+        const worksheet = workbook.Sheets[sheetName];
+        const data = XLSX.utils.sheet_to_json(worksheet);
 
-    //     let actualData = [];
+        let actualData = [];
 
-    //     data.forEach((item) => {
-    //       if (item.Owner == owner) {
-    //         actualData.push(item);
-    //       }
-    //     });
+        data.forEach((item) => {
+          if (item.Owner == owner) {
+            actualData.push(item);
+          }
+        });
 
-    //     res.json(actualData);
-    //   })
-    //   .catch((error) => {
-    //     console.error(error);
-    //   });
+        res.json(actualData);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   });
-
-  return res.json({
-    "jitendra": "test"
-  })
-
-
-
-
 });
 
 module.exports = router;
