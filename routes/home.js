6f64e24375675b2561a78f7a316c1984d9bed821
KhilarJitendra <jitendra.khilar@akqa.com>
2023-03-10 02:07:41 +0530

    connection28

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const request = require("request");
-const csv = require("csv-parser");
 const fs = require("fs-extra");
 const XLSX = require("xlsx");
 // const { JSON } = require("express");
@@ -19,11 +17,16 @@ router.get("/", async (req, res, next) => {
 
   // const configData = JSON.parse(jsonData);
 
+  let url = '';
+  let owner = '';
+
   fs.readFile('extractor-config.json')
   .then(data => {
     console.log('bufferdata......', JSON.parse(data));
     const parseddata = JSON.parse(data);
-    console.log(parseddata)
+    url = parseddata.sheets[0].url;
+    owner = parseddata.assignedTo
+    console.log(owner)
   })
   .catch(err => {
     console.error(err);
@@ -35,13 +38,12 @@ router.get("/", async (req, res, next) => {
 
   // console.log('configData..',jsonData)
 
-  const axios = require("axios");
-  const XLSX = require("xlsx");
+
   // const url = configData.sheets[0].url;
   // const owner = configData.assignedTo;
 
   axios
-    .get("https://docs.google.com/spreadsheets/d/e/2PACX-1vTll1DTciIbS7lsdkjYmgrFnhnD4AJlmtq8u9AZOOAJDWdHzpkdVVbMRXQPNVGXvyFvzMNvv1C7O5JO/pubhtml", {
+    .get(url, {
       responseType: "arraybuffer",
     })
     .then((response) => {
@@ -53,7 +55,7 @@ router.get("/", async (req, res, next) => {
       let actualData = [];
 
       data.forEach((item) => {
-        if (item.Owner == "Jitendra") {
+        if (item.Owner == owner) {
           actualData.push(item);
         }
       });
