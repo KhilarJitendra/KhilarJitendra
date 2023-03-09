780fb7ee33c35fc716fead4d6e0d299e1f6dbad9
KhilarJitendra <jitendra.khilar@akqa.com>
2023-03-10 00:16:30 +0530

    connectrion13

--- routes/home.js
+++ routes/home.js
@@ -9,40 +9,44 @@ const axios = require("axios");
 
 router.get("/", async (req, res, next) => {
   fs.readFile("extractor-config.json", "utf8", (err, data) => {
-    const configData = JSON.parse(data);
+
+    return res.json(data);
+
+
+    // const configData = JSON.parse(data);
 
     // Read the Google Sheet
 
-    const axios = require("axios");
-    const XLSX = require("xlsx");
-    const url = configData.sheets[0].url;
-    const owner = configData.assignedTo;
+    // const axios = require("axios");
+    // const XLSX = require("xlsx");
+    // const url = configData.sheets[0].url;
+    // const owner = configData.assignedTo;
 
-    axios
-      .get(url, {
-        responseType: "arraybuffer",
-      })
-      .then((response) => {
-        const workbook = XLSX.read(response.data, { type: "buffer" });
-        const sheetName = workbook.SheetNames[0];
-        const worksheet = workbook.Sheets[sheetName];
-        const data = XLSX.utils.sheet_to_json(worksheet);
+    // axios
+    //   .get(url, {
+    //     responseType: "arraybuffer",
+    //   })
+    //   .then((response) => {
+    //     const workbook = XLSX.read(response.data, { type: "buffer" });
+    //     const sheetName = workbook.SheetNames[0];
+    //     const worksheet = workbook.Sheets[sheetName];
+    //     const data = XLSX.utils.sheet_to_json(worksheet);
 
-        let actualData = [];
+    //     let actualData = [];
 
-        data.forEach((item) => {
-          if (item.Owner == owner) {
-            actualData.push(item);
-          }
-        });
+    //     data.forEach((item) => {
+    //       if (item.Owner == owner) {
+    //         actualData.push(item);
+    //       }
+    //     });
 
-        return res.json(actualData);
+    //     return res.json(actualData);
 
         
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    //   })
+    //   .catch((error) => {
+    //     console.error(error);
+    //   });
   });
 });
 
