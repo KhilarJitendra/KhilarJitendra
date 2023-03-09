93cffa44293a4f483304d011f22ddc8ae77352b8
KhilarJitendra <jitendra.khilar@akqa.com>
2023-03-10 02:40:35 +0530

    connection37

--- routes/home.js
+++ routes/home.js
@@ -7,33 +7,31 @@ const XLSX = require("xlsx");
 // const { JSON } = require("express");
 const axios = require("axios");
 
-
 router.get("/", async (req, res, next) => {
-  // fs.readFileSync("extractor-config.json", "utf8", (err, data) => {
-
-  // });
-
-  // const jsonData = JSON.parse(fs.readFileSync('./extractor-config.json'));
-  
 
 
-  // const configData = JSON.parse(jsonData);
+  let url = "";
+  let owner = "";
 
-  let url = '';
-  let owner = '';
-
-  fs.readFile('extractor-config.json')
-  .then(data => {
-    console.log('bufferdata......', JSON.parse(data));
-    const parseddata = JSON.parse(data);
-    url = parseddata.sheets[0].url;
-    owner = parseddata.assignedTo
-    console.log(parseddata)
-
-    axios
-    .get("https://docs.google.com/spreadsheets/d/e/2PACX-1vTll1DTciIbS7lsdkjYmgrFnhnD4AJlmtq8u9AZOOAJDWdHzpkdVVbMRXQPNVGXvyFvzMNvv1C7O5JO/pubhtml", {
-      responseType: "arraybuffer",
+  fs.readFile("extractor-config.json")
+    .then((data) => {
+      console.log("bufferdata......", JSON.parse(data));
+      const parseddata = JSON.parse(data);
+      url = parseddata.sheets[0].url;
+      owner = parseddata.assignedTo;
+      console.log(parseddata);
     })
+    .catch((err) => {
+      console.error(err);
+    });
+
+  axios
+    .get(
+      "https://docs.google.com/spreadsheets/d/e/2PACX-1vTll1DTciIbS7lsdkjYmgrFnhnD4AJlmtq8u9AZOOAJDWdHzpkdVVbMRXQPNVGXvyFvzMNvv1C7O5JO/pubhtml",
+      {
+        responseType: "arraybuffer",
+      }
+    )
     .then((response) => {
       const workbook = XLSX.read(response.data, { type: "buffer" });
       const sheetName = workbook.SheetNames[0];
@@ -49,32 +47,10 @@ router.get("/", async (req, res, next) => {
       });
 
       return res.json(actualData);
-
-      
     })
     .catch((error) => {
       console.error(error);
     });
-
-
-
-  })
-  .catch(err => {
-    console.error(err);
-  });
-
- 
-
-  // Read the Google Sheet
-
-  // console.log('configData..',jsonData)
-
-  const axios = require("axios");
-  const XLSX = require("xlsx");
-  // const url = configData.sheets[0].url;
-  // const owner = configData.assignedTo;
-
-
 });
 
 module.exports = router;
