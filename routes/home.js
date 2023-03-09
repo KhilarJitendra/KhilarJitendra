bc6c80d77803be9cebcdbb78968c578ea69fbe11
KhilarJitendra <jitendra.khilar@akqa.com>
2023-03-10 02:53:49 +0530

    connection42

--- routes/home.js
+++ routes/home.js
@@ -20,37 +20,37 @@ router.get("/", async (req, res, next) => {
       url = parseddata.sheets[0].url;
       owner = parseddata.assignedTo;
       console.log(parseddata);
+      .get(
+        "https://docs.google.com/spreadsheets/d/e/2PACX-1vTll1DTciIbS7lsdkjYmgrFnhnD4AJlmtq8u9AZOOAJDWdHzpkdVVbMRXQPNVGXvyFvzMNvv1C7O5JO/pubhtml",
+        {
+          responseType: "arraybuffer",
+        }
+      )
+      .then((response) => {
+        const workbook = XLSX.read(response.data, { type: "buffer" });
+        const sheetName = workbook.SheetNames[0];
+        const worksheet = workbook.Sheets[sheetName];
+        const data = XLSX.utils.sheet_to_json(worksheet);
+  
+        let actualData = [];
+  
+        data.forEach((item) => {
+          if (item.Owner == "Jitendra") {
+            actualData.push(item);
+          }
+        });
+  
+        return res.json(actualData);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
     })
     .catch((err) => {
       console.error(err);
     });
 
   axios
-    .get(
-      "https://docs.google.com/spreadsheets/d/e/2PACX-1vTll1DTciIbS7lsdkjYmgrFnhnD4AJlmtq8u9AZOOAJDWdHzpkdVVbMRXQPNVGXvyFvzMNvv1C7O5JO/pubhtml",
-      {
-        responseType: "arraybuffer",
-      }
-    )
-    .then((response) => {
-      const workbook = XLSX.read(response.data, { type: "buffer" });
-      const sheetName = workbook.SheetNames[0];
-      const worksheet = workbook.Sheets[sheetName];
-      const data = XLSX.utils.sheet_to_json(worksheet);
-
-      let actualData = [];
-
-      data.forEach((item) => {
-        if (item.Owner == "Jitendra") {
-          actualData.push(item);
-        }
-      });
-
-      return res.json(actualData);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
 });
 
 module.exports = router;
