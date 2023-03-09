98e0a1bc7ee9ff330cf943910c2c1d1bf08e03ff
KhilarJitendra <jitendra.khilar@akqa.com>
2023-03-10 02:58:02 +0530

    connnection45

--- routes/home.js
+++ routes/home.js
@@ -13,43 +13,43 @@ router.get("/", async (req, res, next) => {
   let url = "";
   let owner = "";
 
-  fs.readFile("extractor-config.json")
-    .then((data) => {
-      console.log("bufferdata......", JSON.parse(data));
-      const parseddata = JSON.parse(data);
-      url = parseddata.sheets[0].url;
-      owner = parseddata.assignedTo;
-      console.log(parseddata);
-      axios
-        .get(
-          "https://docs.google.com/spreadsheets/d/e/2PACX-1vTll1DTciIbS7lsdkjYmgrFnhnD4AJlmtq8u9AZOOAJDWdHzpkdVVbMRXQPNVGXvyFvzMNvv1C7O5JO/pubhtml",
-          {
-            responseType: "arraybuffer",
-          }
-        )
-        .then((response) => {
-          const workbook = XLSX.read(response.data, { type: "buffer" });
-          const sheetName = workbook.SheetNames[0];
-          const worksheet = workbook.Sheets[sheetName];
-          const data = XLSX.utils.sheet_to_json(worksheet);
-    
-          let actualData = [];
-    
-          data.forEach((item) => {
-            if (item.Owner == "Jitendra") {
-              actualData.push(item);
-            }
-          });
-    
-          return res.json(actualData);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
+  const exctractdata =  fs.readFile("extractor-config.json");
+
+  const parseddata = JSON.parse(exctractdata);
+
+  // console.log("bufferdata......", JSON.parse(data));
+  // const parseddata = JSON.parse(data);
+  url = parseddata.sheets[0].url;
+  owner = parseddata.assignedTo;
+  console.log(parseddata);
+  axios
+    .get(
+      "https://docs.google.com/spreadsheets/d/e/2PACX-1vTll1DTciIbS7lsdkjYmgrFnhnD4AJlmtq8u9AZOOAJDWdHzpkdVVbMRXQPNVGXvyFvzMNvv1C7O5JO/pubhtml",
+      {
+        responseType: "arraybuffer",
+      }
+    )
+    .then((response) => {
+      const workbook = XLSX.read(response.data, { type: "buffer" });
+      const sheetName = workbook.SheetNames[0];
+      const worksheet = workbook.Sheets[sheetName];
+      const data = XLSX.utils.sheet_to_json(worksheet);
+
+      let actualData = [];
+
+      data.forEach((item) => {
+        if (item.Owner == "Jitendra") {
+          actualData.push(item);
+        }
+      });
+
+      return res.json(actualData);
     })
-    .catch((err) => {
-      console.error(err);
+    .catch((error) => {
+      console.error(error);
     });
+
+ 
 
 });
 
