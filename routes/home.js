33a84e18d0514ef17825cd1ffe57327ccb3833a6
KhilarJitendra <jitendra.khilar@akqa.com>
2023-03-09 23:31:24 +0530

    test connection

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const request = require("request");
-const csv = require("csv-parser");
 const fs = require("fs");
 const XLSX = require("xlsx");
 const { json } = require("express");
@@ -18,29 +16,31 @@ router.get("/", async (req, res, next) => {
     const url = configData.sheets[0].url;
     const owner = configData.assignedTo;
 
-    axios
-      .get(url, {
-        responseType: "arraybuffer",
-      })
-      .then((response) => {
-        const workbook = XLSX.read(response.data, { type: "buffer" });
-        const sheetName = workbook.SheetNames[0];
-        const worksheet = workbook.Sheets[sheetName];
-        const data = XLSX.utils.sheet_to_json(worksheet);
+    console.log('Dataa are...',data)
 
-        let actualData = [];
+    // axios
+    //   .get(url, {
+    //     responseType: "arraybuffer",
+    //   })
+    //   .then((response) => {
+    //     const workbook = XLSX.read(response.data, { type: "buffer" });
+    //     const sheetName = workbook.SheetNames[0];
+    //     const worksheet = workbook.Sheets[sheetName];
+    //     const data = XLSX.utils.sheet_to_json(worksheet);
 
-        data.forEach((item) => {
-          if (item.Owner == owner) {
-            actualData.push(item);
-          }
-        });
+    //     let actualData = [];
 
-        res.json(actualData);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    //     data.forEach((item) => {
+    //       if (item.Owner == owner) {
+    //         actualData.push(item);
+    //       }
+    //     });
+
+    //     res.json(actualData);
+    //   })
+    //   .catch((error) => {
+    //     console.error(error);
+    //   });
   });
 });
 
