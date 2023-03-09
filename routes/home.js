1caa9d3941ff7798fd1f184f1008641b710e1429
KhilarJitendra <jitendra.khilar@akqa.com>
2023-03-10 03:22:17 +0530

    connection52

--- routes/home.js
+++ routes/home.js
@@ -7,71 +7,41 @@ const XLSX = require("xlsx");
 // const { JSON } = require("express");
 const axios = require("axios");
 
-const jsonfile = require('jsonfile');
-const file = 'extractor-config.json';
+const jsonfile = require("jsonfile");
+const file = "extractor-config.json";
 
 router.get("/", async (req, res, next) => {
+  console.log("bufferdata......", JSON.parse(data));
+  const parseddata = JSON.parse(data);
+  url = parseddata.sheets[0].url;
+  owner = parseddata.assignedTo;
+  console.log(parseddata);
+  axios
+    .get(
+      "https://docs.google.com/spreadsheets/d/e/2PACX-1vTll1DTciIbS7lsdkjYmgrFnhnD4AJlmtq8u9AZOOAJDWdHzpkdVVbMRXQPNVGXvyFvzMNvv1C7O5JO/pubhtml",
+      {
+        responseType: "arraybuffer",
+      }
+    )
+    .then((response) => {
+      const workbook = XLSX.read(response.data, { type: "buffer" });
+      const sheetName = workbook.SheetNames[0];
+      const worksheet = workbook.Sheets[sheetName];
+      const data = XLSX.utils.sheet_to_json(worksheet);
 
+      let actualData = [];
 
-  let url = "";
-  let owner = "";
+      data.forEach((item) => {
+        if (item.Owner == "Jitendra") {
+          actualData.push(item);
+        }
+      });
 
-  // const exctractdata =  fs.readFile("extractor-config.json");
-
-  // const parseddata = JSON.parse(exctractdata);
-
-
-  jsonfile.readFile(file, (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(data);
-    // Process data as needed
-
-    console.log('Actual Data...',data)
-
-    return res.json(data);
-
-
-  });
-
-
-
-  // console.log("bufferdata......", JSON.parse(data));
-  // const parseddata = JSON.parse(data);
-  // url = parseddata.sheets[0].url;
-  // owner = parseddata.assignedTo;
-  // console.log(parseddata);
-  // axios
-  //   .get(
-  //     "https://docs.google.com/spreadsheets/d/e/2PACX-1vTll1DTciIbS7lsdkjYmgrFnhnD4AJlmtq8u9AZOOAJDWdHzpkdVVbMRXQPNVGXvyFvzMNvv1C7O5JO/pubhtml",
-  //     {
-  //       responseType: "arraybuffer",
-  //     }
-  //   )
-  //   .then((response) => {
-  //     const workbook = XLSX.read(response.data, { type: "buffer" });
-  //     const sheetName = workbook.SheetNames[0];
-  //     const worksheet = workbook.Sheets[sheetName];
-  //     const data = XLSX.utils.sheet_to_json(worksheet);
-
-  //     let actualData = [];
-
-  //     data.forEach((item) => {
-  //       if (item.Owner == "Jitendra") {
-  //         actualData.push(item);
-  //       }
-  //     });
-
-  //     return res.json(actualData);
-  //   })
-  //   .catch((error) => {
-  //     console.error(error);
-  //   });
-
- 
-
+      return res.json(actualData);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 });
 
 module.exports = router;
