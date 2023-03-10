ca8e16126b9c266663e134f88ec10bf694db1a76
KhilarJitendra <jitendra.khilar@akqa.com>
2023-03-10 14:59:05 +0530

    import replace with require

--- index.js
+++ index.js
@@ -1,13 +1,50 @@
 // Import packages
-const express = require("express");
-const home = require("./routes/home");
+// const express = require("express");
+import express from "express";
+
+
+import xlsx from "xlsx";
+import axios from "axios";
+import configfile from "./config.json" assert { type: "json" };
+
 
 // Middlewares
 const app = express();
 app.use(express.json());
 
-// Routes
-app.use("/home", home);
+app.get("/", (req, res, next) => {
+
+    console.log('JSON DATA...',configfile.assignedTo)
+    const assignedTo = configfile.assignedTo;
+    const url = configfile.sheets[0].url;
+
+  axios
+    .get(
+        url,
+      {
+        responseType: "arraybuffer",
+      }
+    )
+    .then((response) => {
+      const workbook = xlsx.read(response.data, { type: "buffer" });
+      const sheetName = workbook.SheetNames[0];
+      const worksheet = workbook.Sheets[sheetName];
+      const data = xlsx.utils.sheet_to_json(worksheet);
+
+      let actualData = [];
+
+      data.forEach((item) => {
+        if (item.Owner == assignedTo) {
+          actualData.push(item);
+        }
+      });
+
+      res.json(actualData);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
 
 // connection
 const port = process.env.PORT || 9009;
--- routes/home.js
+++ routes/home.js
@@ -1,65 +1,61 @@
-const express = require("express");
-const router = express.Router();
-const request = require("request");
-const csv = require("csv-parser");
-const fs = require("fs-extra");
-const XLSX = require("xlsx");
-// const { json } = require("express");
-const axios = require("axios");
+// // const express = require("express");
+// import express from 'express';
+// const router = express.Router();
+
+// // const fs = require("fs");
+// import fs from 'fs';
+
+// // const xlsx = require("xlsx");
+// import xlsx from 'fs';
+
+// import axios from 'axios';
 
 
 
+// import file from '../config.json';
 
+// // router.get("/", async (req, res, next) => {
+// //   fs.readFile("./config.json", "utf8", (err, data) => {
+// //     const configData = JSON.parse(data);
 
+// //     console.log('Import File,,,,',file);
 
-router.get("/", async (req, res, next) => {
+// //     // Read the Google Sheet
+
   
+// //     const url = configData.sheets[0].url;
+// //     const owner = configData.assignedTo;
+
+// //     axios
+// //       .get(url, {
+// //         responseType: "arraybuffer",
+// //       })
+// //       .then((response) => {
+// //         const workbook = xlsx.read(response.data, { type: "buffer" });
+// //         const sheetName = workbook.SheetNames[0];
+// //         const worksheet = workbook.Sheets[sheetName];
+// //         const data = xlsx.utils.sheet_to_json(worksheet);
+
+// //         let actualData = [];
+
+// //         data.forEach((item) => {
+// //           if (item.Owner == owner) {
+// //             actualData.push(item);
+// //           }
+// //         });
+
+// //         res.json(actualData);
+// //       })
+// //       .catch((error) => {
+// //         console.error(error);
+// //       });
+// //   });
+// // });
+
+// // module.exports = router;
 
 
-  const jsondata = JSON.parse(fs.readFileSync('../config.json'));
-  console.log('JSON DATA',jsondata.assignedTo)
+export default function Home() {
+  return a + b;
+}
 
-  // const workbook = await XLSX.readFile('https://docs.google.com/spreadsheets/d/14N2Ob5-fMHDWbU9p26mpBVuVupV2WnCGsrAC5pnWxuY/gviz/tq?tqx=out:csv&sheet=Task%20List');
-  // const worksheet = workbook.Sheets[sheetName];
-  // const data = XLSX.utils.sheet_to_json(worksheet);
-
-  // console.log('actual data',data)
-
-  // fs.readFile('./config.json','utf-8',(err,data) => {
-  //   // const parseddata = JSON.parse(JSON.stringify(data));
-  //   console.log('JSON data',typeof(data))
-  //   // owner = parseddata.assignedTo;
-  //   if(err) throw err;
-  //   // console.log(owner);
-  // })
-
-
-  await axios
-    .get(
-      "https://docs.google.com/spreadsheets/d/e/2PACX-1vTll1DTciIbS7lsdkjYmgrFnhnD4AJlmtq8u9AZOOAJDWdHzpkdVVbMRXQPNVGXvyFvzMNvv1C7O5JO/pubhtml",
-      {
-        responseType: "arraybuffer",
-      }
-    )
-    .then((response) => {
-      const workbook = XLSX.read(response.data, { type: "buffer" });
-      const sheetName = workbook.SheetNames[0];
-      const worksheet = workbook.Sheets[sheetName];
-      const data = XLSX.utils.sheet_to_json(worksheet);
-
-      let actualData = [];
-
-      data.forEach((item) => {
-        if (item.Owner == jsondata.assignedTo) {
-          actualData.push(item);
-        }
-      });
-
-      return res.json(actualData);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-});
-
-module.exports = router;
